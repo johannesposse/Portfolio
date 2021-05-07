45140ab0695b3d89f380d16d889ffe75d1d8c025
Johannes Posse <johannesposse@gmail.com>
2021-05-07 17:03:37 +0200

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnProject = document.querySelector("#btnProject")
+
+btnProject.addEventListener("click", function(){
+    let showInfo = document.querySelector(".showInfo").style.top = "0" //top: -100px;
+    // let showInfo = document.querySelector(".showInfo").style.display = "flex" //top: -100px;
+    console.log(document.querySelector(".showInfo").style.visibility)
+    
+})
